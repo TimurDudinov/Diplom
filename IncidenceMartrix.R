b2ae9d4b299f5adcf3a876cb8f1abd475b0f95ba
TimurDudinov <62845820+TimurDudinov@users.noreply.github.com>
2020-04-14 09:01:43 +0300

    Update IncidenceMartrix.R

--- IncidenceMartrix.R
+++ IncidenceMartrix.R
@@ -2,51 +2,44 @@ library(Matrix)
 library(installr)
 #N - number of vertices = number of rows in incidence matrix
 #M - number of edges = number of columns in incidence matrix
-#plus -number of outcoming edges from every vertices
 N<-
-plus<-
-Vertices<-c(1:N)
-AmountEnds<-sample(1:(N-1),1)#amount end vertices
-NumberEnds<-sample(2:N,AmountEnds)#numbers of end vertices
-CountNotEnds<-length(Vertices[-(NumberEnds)])
-# matrix there are some end vertices
-GenIncidenMatrix <- function(N, plus) { 
-    M <- N * plus
+OutOfDegree<-
+GenIncidenMatrix <- function(N, OutOfDegree) { 
+    M <- N * OutOfDegree
     A <- matrix(0, nrow = N, ncol = M)
     edgesIndex <- 1
-   
-    for (i in 1:CountNotEnds){
-        if (Vertices[-(NumberEnds)][i]!=N)
+    for (i in 1:N)
+    {
+        if (i!=N)
         {
-        A[Vertices[-(NumberEnds)][i], edgesIndex] <- 1
-        A[Vertices[-(NumberEnds)][i]+1, edgesIndex] <- -1
-        edgesIndex <- edgesIndex + 1
+            A[i,edgesIndex]<-1
+            A[i+1,edgesIndex]<- -1
+            edgesIndex<- edgesIndex+1
         }
     }
-    targets <- c()
-    for (j in 1:N) {
-        targets[j] <- j
-    }
-    for (i in 1:CountNotEnds) {
-        targets <- targets[-1]
-        for (j in 1:(plus - 1)) {
-            if (is.empty(targets) != T) {
-                A[Vertices[-(NumberEnds)][i], edgesIndex] <- 1
-                target <- targets[sample(1:length(targets), 1)]
-                A[target, edgesIndex] <- -1
-                edgesIndex <- edgesIndex + 1
-            }
+    targets <- c(1:N)
+    for (i in 1:N)
+    { targets<-targets[-1]
+    for (j in 1:(OutOfDegree-1))
+    {
+        if(is.empty(targets)!=T)
+        {
+            A[i,edgesIndex]<-1
+            target<-targets[sample(1:length(targets),1)]
+            A[target,edgesIndex]<- -1
+            edgesIndex<- edgesIndex+1
         }
     }
-    #delete zero columns
-    B <- A
+    }
+    #удаление нулевых столбцов
+    B<-A
     k <- 0
-    for (i in 1:length(A[1, ])) {
-        if (sum(A[, i] == 0) == length(A[, 1])) {
-            B <- B[, - i + k]
+    for (i in 1:length(A[1,])){
+        if (sum(A[, i] == 0) == length(A[,1])) {
+            B <- B[,-i + k]
             k <- k + 1
         }
     }
-    A <- B
+    A<-B
     return(A)
 }
