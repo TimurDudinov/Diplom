bd47486470dc94596c9de1c53f6b9837983648fc
TimurDudinov <62845820+TimurDudinov@users.noreply.github.com>
2020-04-15 03:59:10 +0300

    Update IncidenceMartrix.R

--- IncidenceMartrix.R
+++ IncidenceMartrix.R
@@ -2,23 +2,27 @@ library(Matrix)
 library(installr)
 #N - number of vertices = number of rows in incidence matrix
 #M - number of edges = number of columns in incidence matrix
+#OutOfDegree -number of outcoming edges from every vertices
 N<-
 OutOfDegree<-
+
+# get matrix where some end vertices 
 GenIncidenMatrix <- function(N, OutOfDegree) { 
     M <- N * OutOfDegree
+    AmountEnds<-sample(1:(N-1),1)
     A <- matrix(0, nrow = N, ncol = M)
+    
     edgesIndex <- 1
-    for (i in 1:N)
+    for (i in 1:(N-AmountEnds))
     {
-        if (i!=N)
-        {
+        
             A[i,edgesIndex]<-1
             A[i+1,edgesIndex]<- -1
             edgesIndex<- edgesIndex+1
-        }
+        
     }
     targets <- c(1:N)
-    for (i in 1:N)
+    for (i in 1:(N-AmountEnds))
     { targets<-targets[-1]
     for (j in 1:(OutOfDegree-1))
     {
@@ -41,5 +45,74 @@ GenIncidenMatrix <- function(N, OutOfDegree) {
         }
     }
     A<-B
+    rm(B)
+    rm(k)
+    Vertices<-c(1:N)
+    Ends<-Vertices[-(1:(N-AmountEnds))]
+    NumberEnds<-c(sample(2:N,AmountEnds))
+    NumberEnds<-NumberEnds[order(NumberEnds)]#order in increasing
+#replace end rows    
+    tch<-c()
+    tch1<-c()
+    for ( i in 1:AmountEnds)
+    {
+        if ((Ends[i] %in% NumberEnds) == F)
+        {
+            tch[i]<-Ends[i]
+        }
+        if ((NumberEnds[i] %in% Ends)==F)
+        {
+            tch1[i]<-NumberEnds[i] 
+        }
+    }
+    tch<-tch[!is.na(tch)]
+    tch1<-tch1[!is.na(tch1)]
+    
+    for (i in 1:AmountEnds)
+    {
+        copy<-A[tch[i],]
+        A[tch[i],]<-A[tch1[i],]
+        A[tch1[i],]<-copy
+    }
     return(A)
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
